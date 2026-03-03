5beb3cf837614d09cac6a67d9d51580041b6c717
ShaneCruiseClare <114066730+ShaneCruiseClare@users.noreply.github.com>
2026-03-03 19:12:26 +0000

    Add files via upload
    auto thresh added

--- opencv_starter.py
+++ opencv_starter.py
@@ -12,6 +12,49 @@ def threshold(img,thresh):
                 img[x,y] = 0
     return img
 
+#get img and analysis the peaks of the pixels using otsu's method
+def auto_threshold(img):
+
+    hist = histogram(img)
+
+    #get the total number of pixels
+    total = img.shape[0] * img.shape[1]
+    sum = 0
+
+    for t in range(256):
+        sum += t * hist[t]
+
+    sumB = 0
+    wB = 0
+    wF = 0
+    varMax = 0
+    threshold = 0
+
+    for t in range(256):
+        wB += hist[t]
+        #Weight background
+        if wB == 0:
+            continue
+        #Weight forground
+        wF = total - wB
+        if wF == 0:
+            break
+        sumB += t * hist[t]
+        #Mean background
+        mB = sumB / wB
+        #Mean background
+        mF = (sum - sumB) / mF
+
+        # calculate the betwwen class variance 
+        varBetween = wB * wF * (mB - mF) * (mB - mF)
+
+        #check if new max has been found 
+        if varBetween > varMax:
+            varMax = varBetween
+            threshold = t
+        
+
+
 #create histogram
 #find threshold of histogram of image *example set back 50 pixels from peak
 def histogram(img):
@@ -23,11 +66,15 @@ def histogram(img):
     return hist
 
 #create a label function that prints text pass or fail 
+#def labelPassOrFails():
+    #passLabel = np.array([]}    
+
+
 
 #read in an image into memory
 for imgList in range(1,16):
     img = cv.imread('C:/Users/shane/TUD/Computer Vision/images/Oring' + str(imgList) + '.jpg',0)
-    thresh = 100
+    thresh = auto_threshold(img)
     hist = histogram(img)
     plt.plot(hist)
     plt.show()
