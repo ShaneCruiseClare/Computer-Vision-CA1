81cf5343602a59aa836cc5226d0d6be926581c83
ShaneCruiseClare <114066730+ShaneCruiseClare@users.noreply.github.com>
2026-03-09 11:28:06 +0000

    Add files via upload

--- opencv_starter.py
+++ opencv_starter.py
@@ -10,15 +10,23 @@ def threshold(img,thresh):
             else:
                 img[x,y] = 0
     return img
+#create histogram
+#find threshold of histogram of image *example set back 50 pixels from peak
+def histogram(img):
+    hist = np.zeros(256)
+    for x in range(0, img.shape[0]):
+        for y in range(0, img.shape[1]):
+            hist[img[x,y]] += 1
 
+    return hist
 #get img and analysis the peaks of the pixels using otsu's method
 def auto_threshold(img):
 
-    hist = histogram(img)
-    #hist = np.zeros(256)
-    #for x in range(0, img.shape[0]):
-        #for y in range(0, img.shape[1]):
-            #hist[img[x,y]] += 1
+    #hist = histogram(img)
+    hist = np.zeros(256)
+    for x in range(0, img.shape[0]):
+        for y in range(0, img.shape[1]):
+            hist[img[x,y]] += 1
 
     #get the total number of pixels
     total = img.shape[0] * img.shape[1]
@@ -31,7 +39,7 @@ def auto_threshold(img):
     wB = 0
     wF = 0
     varMax = 0
-    threshold = 0
+    threshold = 3
 
     for t in range(256):
         wB += hist[t]
@@ -58,29 +66,27 @@ def auto_threshold(img):
 
         return threshold
         
-
-
-#create histogram
-#find threshold of histogram of image *example set back 50 pixels from peak
-def histogram(img):
-    hist = np.zeros(256)
-    for x in range(0, img.shape[0]):
-        for y in range(0, img.shape[1]):
-            hist[img[x,y]] += 1
-
-    return hist
-
 #create a label function that prints text pass or fail 
 #def labelPassOrFails():
-    #passLabel = np.array([]}    
+    #passLabel     
 
+#needs two elements, using an array to be able to show 
+def binaryMorp():
+    #need to get the img and use the array to fill in the different gaps on the orings
+    rows, cols = (8, 8)
+    
+    arr = [[0 for i in range(cols)] for j in range(rows)]
 
+    arr[0][0] = 1
+    for row in arr:
+        print(row)
 
 #read in an image into memory
 for imgList in range(1,16):
     img = cv.imread('C:/Users/shane/TUD/Computer Vision/images/Oring' + str(imgList) + '.jpg',0)
     thresh = auto_threshold(img)
     hist = histogram(img)
+    bM = binaryMorp()
     plt.plot(hist)
     plt.show()
     bw = threshold(img,thresh)
